509180d1cb7767963f3a9aae2c019eb593d27c9d
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-07-09 17:44:03 -0500

    first test was succesful.

--- led.py
+++ led.py
@@ -1,17 +1,12 @@
+LED_PIN = 17
+
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(17, GPIO.OUT)
-
-try:
-    while True:
-        GPIO.output(17, GPIO.HIGH)  # Turn LED on
-        time.sleep(1)               # Wait for 1 second
-        GPIO.output(17, GPIO.LOW)   # Turn LED off
-        time.sleep(1)               # Wait for 1 second
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup() 
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.output(LED_PIN, GPIO.HIGH)
+time.sleep(10)
+GPIO.output(LED_PIN, GPIO.LOW)
+GPIO.cleanup()
+# This code turns on an LED connected to GPIO pin 17 for 10 seconds and then turns it off.
