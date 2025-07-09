bde13a482e514aedc62c5b1570084b2d23def263
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-07-09 17:45:09 -0500

    cleaned up code.

--- led.py
+++ led.py
@@ -4,9 +4,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(LED_PIN, GPIO.OUT)
+
 GPIO.output(LED_PIN, GPIO.HIGH)
+
 time.sleep(10)
+
 GPIO.output(LED_PIN, GPIO.LOW)
+
 GPIO.cleanup()
 # This code turns on an LED connected to GPIO pin 17 for 10 seconds and then turns it off.
